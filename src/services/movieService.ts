a4ac4e150a49850c636de1f5cf3438ccb89dbd78
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-05-30 13:57:25 -0600

    feat: add movie services

---
+++ src/services/movieService.ts
@@ -0,0 +1,23 @@
+import { MovieTypes } from 'types/movie.types'
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: import.meta.env.VITE_API_KEY
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace MovieService {
+  export const getMovies = async(genreId: number) => {
+    const res = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}`, options)
+    const response: MovieTypes.Movies = await res.json()
+    return response
+  }
+
+  export const getCategory = async () => {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list', options)
+    const response: MovieTypes.Genres = await res.json()
+    return response
+  }
+}
