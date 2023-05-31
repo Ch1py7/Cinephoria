83e2e278d41c6ec4b0eab1c71ec65979593d1d7d
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-05-31 15:10:47 -0600

    feat: add movie types

--- src/types/movie.types.d.ts
+++ src/types/movie.types.d.ts
@@ -16,6 +16,60 @@ export namespace MovieTypes {
     vote_average: number
     vote_count: number
   }
+
+  export interface MovieProps {
+    adult: false
+    backdrop_path: '/h8gHn0OzBoaefsYseUByqsmEDMY.jpg'
+    belongs_to_collection: BelongsToCollection[]
+    budget: number
+    genres: GenresProps[]
+    homepage: string
+    id: number
+    imdb_id: string
+    original_language: string
+    original_title: string
+    overview: string
+    popularity: number
+    poster_path: string
+    production_companies: ProductionCompanies[]
+    production_countries: ProductionCountries[]
+    release_date: string
+    revenue: number
+    runtime: number
+    spoken_languages: SpokenLanguages[]
+    status: string
+    tagline: string
+    title: string
+    video: boolean
+    vote_average: number
+    vote_count: number
+  }
+
+  export interface BelongsToCollection {
+    backdrop_path: string
+    id: number
+    name: string
+    poster_path: string
+  }
+
+  export interface SpokenLanguages {
+    english_name: string
+    iso_639_1: string
+    name: string
+  }
+
+  export interface ProductionCompanies {
+    id: number
+    logo_path: string
+    name: string
+    origin_country: string
+  }
+
+  export interface ProductionCountries {
+    iso_3166_1: string
+    name: string
+  }
+
   export interface Movies {
     page: number
     results: MoviesProps[]
