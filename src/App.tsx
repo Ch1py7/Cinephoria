eb3115e0c51e97789309301222e4ea42f55fc408
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-05-31 15:14:18 -0600

    feat: add movie page to apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { HomePage } from './components/HomePage'
+import { MoviePage } from 'components/MoviePage'
 import { FC, ReactElement } from 'react'
-import { Routes, Route } from 'react-router-dom' 
+import { Routes, Route } from 'react-router-dom'
 
 export const App: FC = (): ReactElement => {
   return (
     <Routes>
       <Route path='/' element={<HomePage />}/>
+      <Route path='/movie/:id' element={<MoviePage />}/>
     </Routes>
   )
 }
