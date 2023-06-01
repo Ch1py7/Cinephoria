0d17c1f03064a82e4497ca50095a03e402ac927e
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-06-01 10:04:44 -0600

    feat: add types for data state

--- src/components/MoviePage/MoviePage.component.tsx
+++ src/components/MoviePage/MoviePage.component.tsx
@@ -4,20 +4,12 @@ import * as S from './MoviePage.styles'
 import { MovieService } from 'services'
 import { useParams } from 'react-router-dom' 
 import { NavComponent } from 'components/NavComponent'
+import { MovieTypes } from 'types/movie.types'
 
 export const MoviePage: FC = (): ReactElement => {
-  const [data, setData] = useState({
-    title: '',
-    overview: '',
-    release_date: '',
-    runtime: 0,
-    poster_path: '',
-    vote_average: 0,
-    tagline: '',
-    genres: [{id: 0, name: ''}],
-    'production_companies': [{id: 0, name: ''}]
-  })
-  const [movieDuration, setMovieDuration] = useState<string | []>()
+  const [data, setData] = useState<MovieTypes.MovieProps>()
+
+  const [movieDuration, setMovieDuration] = useState<string>()
 
   const { id } = useParams()
 
@@ -46,26 +38,26 @@ export const MoviePage: FC = (): ReactElement => {
     <S.MoviePage>
       <NavComponent />
       <S.MovieInformation>
-        <S.MovieImage src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} />
+        <S.MovieImage src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`} />
         <div style={{display: 'flex', flexDirection: 'column', gap: '2rem'}}>
-          <S.MovieTitle>{data.title}</S.MovieTitle>
+          <S.MovieTitle>{data?.title}</S.MovieTitle>
           <div>
-            <S.MovieDescription fontSize='1.4rem'><Icon icon='bx:bxs-star' style={{marginRight: '1rem', color: '#FFC107'}}/>{data.vote_average}</S.MovieDescription>
+            <S.MovieDescription fontSize='1.4rem'><Icon icon='bx:bxs-star' style={{marginRight: '1rem', color: '#FFC107'}}/>{data?.vote_average}</S.MovieDescription>
             <S.MovieDescription fontSize='1.4rem'><Icon icon='uiw:time-o' style={{marginRight: '1rem'}}/>{movieDuration}</S.MovieDescription>
-            <S.MovieDescription fontSize='1.4rem'><Icon icon='ph:calendar-bold' style={{marginRight: '1rem'}}/>{data.release_date}</S.MovieDescription>
+            <S.MovieDescription fontSize='1.4rem'><Icon icon='ph:calendar-bold' style={{marginRight: '1rem'}}/>{data?.release_date}</S.MovieDescription>
           </div>
           <S.MovieSubTitle fontSize='2.2rem'>Overview</S.MovieSubTitle>
-          <S.MovieDescription fontSize='1.8rem'>{data.overview}</S.MovieDescription>
+          <S.MovieDescription fontSize='1.8rem'>{data?.overview}</S.MovieDescription>
           <div>
             <S.MovieDescription style={{color: '#8c8c8c'}} fontSize='1.6rem'>Tagline:</S.MovieDescription>
-            <S.MovieDescription style={{color: '#e1e1e1'}} fontSize='1.8rem'>{data.tagline}</S.MovieDescription>
+            <S.MovieDescription style={{color: '#e1e1e1'}} fontSize='1.8rem'>{data?.tagline}</S.MovieDescription>
             <S.MovieDescription style={{marginTop: '2rem', color: '#8c8c8c'}} fontSize='1.6rem'>Genre:</S.MovieDescription>
-            <div style={{display: 'flex', color: '#8c8c8c'}}>{data.genres?.map((genre) =>
+            <div style={{display: 'flex', color: '#8c8c8c'}}>{data?.genres?.map((genre) =>
               <S.GenreDecoration fontSize='1.8rem' style={{marginRight: '1rem'}} key={genre.id}>{genre.name}</S.GenreDecoration>
             )}
             </div>
             <S.MovieDescription style={{marginTop: '2rem', color: '#8c8c8c'}} fontSize='1.6rem'>Created By:</S.MovieDescription>
-            <div style={{display: 'flex', color: '#8c8c8c'}}>{data.production_companies?.map((company) =>
+            <div style={{display: 'flex', color: '#8c8c8c'}}>{data?.production_companies?.map((company) =>
               <S.GenreDecoration fontSize='1.8rem' style={{marginRight: '1rem'}} key={company.id}>{company.name}</S.GenreDecoration>
             )}
             </div>
