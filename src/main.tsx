e1e2d8433a1cba275c8af0b8708997b8e041486d
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-06-02 11:25:31 -0600

    chore: add import for index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { App } from './App.tsx'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
