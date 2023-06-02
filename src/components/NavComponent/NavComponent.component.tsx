a312676c21571004232f22f554b85949a6dbb784
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-06-01 22:46:43 -0600

    feat: add function for show or not the burger options

--- src/components/NavComponent/NavComponent.component.tsx
+++ src/components/NavComponent/NavComponent.component.tsx
@@ -1,8 +1,15 @@
-import { FC, ReactElement } from 'react'
+import { FC, ReactElement, useState } from 'react'
 import * as S from './NavComponent.styles'
 import { Icon } from '@iconify/react'
+import { BurgerOptions } from 'components/BurgerOptions'
 
 export const NavComponent: FC = (): ReactElement => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+
+  const onClickBurger = () => {
+    setIsMenuOpen((prev) => !prev)
+  }
+
   const IconStyles = {
     fontSize: '2.4rem',
     color: '#e4e0e6'
@@ -10,13 +17,15 @@ export const NavComponent: FC = (): ReactElement => {
 
   return (
     <S.NavComponent>
-      <S.NavSections>
-        <S.NavAnchor href='#'>
-          <Icon icon='iconamoon:menu-burger-horizontal' style={IconStyles} />
-        </S.NavAnchor>
-        <S.NavAnchor href='#'>Movies</S.NavAnchor>
-        <S.NavAnchor href='#'>Series</S.NavAnchor>
-      </S.NavSections>
+      <BurgerOptions isMenuOpen={isMenuOpen} onClickBurger={onClickBurger}/>
+      <S.Burger>
+        <Icon
+          icon='iconamoon:menu-burger-horizontal'
+          style={IconStyles}
+          onClick={onClickBurger}
+        />
+        <S.NavAnchor href='/'>Movies</S.NavAnchor>
+      </S.Burger>
       <S.LogoImage src='/logo.png' draggable={false}/>
       <S.NavSections>
         <S.NavAnchor href='#'>
