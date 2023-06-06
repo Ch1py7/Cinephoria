a19489ef6547c1a80afdbfcec0cefb311bc674a7
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-06-06 13:30:10 -0600

    refactor: main carousel and carousel

--- src/components/HomePage/HomePage.component.tsx
+++ src/components/HomePage/HomePage.component.tsx
@@ -1,23 +1,28 @@
 import { FC, ReactElement } from 'react'
 import { MoviesSection } from 'components/MoviesSection'
 import { NavComponent } from '../NavComponent'
-import { MainCarousel } from '../MainCarousel'
 import * as S from './HomePage.styles'
+import { MainCarousel } from 'components/MainCarousel'
 
 export const HomePage: FC = (): ReactElement => {
-  const pictures= [
+  const pictures = [
     '/halloween.webp',
     '/la-extorsion.webp',
     '/shazam.webp',
     '/succession.webp',
     '/supermariobros.webp',
-    '/fantasticbeasts.webp'
+    '/fantasticbeasts.webp',
+    '/evildead.webp'
   ]
   return (
     <>
       <NavComponent />
       <S.TrendingContainer>
-        <MainCarousel pictures={pictures}/>
+        <MainCarousel>
+          {pictures.map((pic, index) => (
+            <img key={index} src={pic} style={{width: 'calc(100vw - 8rem)', height: '100%'}}/>
+          ))}
+        </MainCarousel>
       </S.TrendingContainer>
       <MoviesSection />
     </>
--- src/components/MainCarousel/MainCarousel.component.tsx
+++ src/components/MainCarousel/MainCarousel.component.tsx
@@ -1,25 +1,39 @@
-import { FC, ReactElement } from 'react'
-import Carousel from 'devsgnr-carousel'
+import { FC, ReactElement, useCallback, useMemo, useState, Children, useEffect } from 'react'
+import { arrowLeft } from 'assets'
+import * as S from './MainCarousel.styles'
 
 interface MainCarouselProps {
-  pictures: string[]
+  children: ReactElement | ReactElement[]
 }
 
-export const MainCarousel: FC<MainCarouselProps> = ({ pictures }): ReactElement => {
+export const MainCarousel: FC<MainCarouselProps> = ({ children }): ReactElement => {
+  const [current, setCurrent] = useState<number>(0)
+  const childrenArray = useMemo(() => Children.toArray(children), [children])
+
+  const handleNext = useCallback(() => {
+    setCurrent((prev) => (prev === childrenArray.length - 1 ? 0 : prev + 1))
+  }, [childrenArray])
+
+  const handlePrev = useCallback(() => {
+    setCurrent((prev) => (prev === 0 ? childrenArray.length - 1 : prev - 1))
+  }, [childrenArray])
+
+  useEffect(() => console.log(childrenArray),[])
   return (
-    <Carousel
-      borderRadius={10}
-      timeout={10000}
-      autoPlay={true}
-      pictures={pictures}
-      height="100vh"
-      showThumb={false}
-      thumbnailOutlineColor="#000"
-      thumbnailOutlineStyle="solid"
-      thumbnailOutlineOffset={1}
-      thumbnailOutlineThickness={2}
-      showButton={true}
-      showIndicator={true}
-    />
+    <S.Carousel>
+      <S.CarouselButtonsContainer>
+        <S.CarouselButton onClick={handlePrev}>
+          <img src={arrowLeft} alt='anterior' />
+        </S.CarouselButton>
+        <S.CarouselButton onClick={handleNext}>
+          <img src={arrowLeft} alt='siguiente' />
+        </S.CarouselButton>
+      </S.CarouselButtonsContainer>
+      <S.CarouselList items={childrenArray.length} current={current}>
+        {childrenArray.map((child, index) => (
+          <S.CarouselItem key={index}>{child}</S.CarouselItem>
+        ))}
+      </S.CarouselList>
+    </S.Carousel>
   )
 }
